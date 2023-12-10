c26bd8462d157d9cd11feafb3cfdcc46c36f2194
alisha <github@ahills.co>
2023-12-10 15:49:27 +0800

    Adding MyProfile html page which loads the users profile information and their stories. Created MyProfileView, and updated URLS

--- she_codes_news/users/urls.py
+++ she_codes_news/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CreateAccountView
+from .views import CreateAccountView, MyProfileView
 
 app_name ='users'
 
 urlpatterns = [
     path('create-account/', CreateAccountView.as_view(), name='createAccount'),
+    path('my-profile/', MyProfileView.as_view(),name='myProfile' )
 ]
     

ce71f0abbdfe8b8a4fbe45d5506a6d436738f35e
alisha <github@ahills.co>
2023-12-10 10:59:11 +0800

    updated admin in news and users to include administration of news stories + users

--- she_codes_news/users/admin.py
+++ she_codes_news/users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import CustomUser
 
 # Register your models here.
+@admin.register(CustomUser)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username','password')#,'First Name','Last Name','Bio')
+    # date_hierarchy = 'created' 
+    # search_fields = ('author')

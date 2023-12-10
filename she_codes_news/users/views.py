82c543f2d95b0ecdb51cb601ee0cd32e561d6e5f
alisha <github@ahills.co>
2023-12-10 14:15:01 +0800

    correct version of django in virtenv which has resolved logout issue

--- she_codes_news/users/views.py
+++ she_codes_news/users/views.py
@@ -7,9 +7,7 @@ from django.views.generic.edit import CreateView
 from django.views import generic
 from .models import CustomUser
 from .forms import CustomUserCreationForm
-# from django.contrib.auth import get_user_model
 
-# User = get_user_model()
 class CreateAccountView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
